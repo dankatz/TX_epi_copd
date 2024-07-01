5bce70b6baf44468b99793193a4bfdf26b985e19
Daniel Katz <dsk856@PRODUCTION>
2024-07-01 16:38:09 -0500

    correcting some problems inherited from someone else's script for combining THCIC data that resulted in many COPD cases from 2018 being omitted

--- THCIC_assembly_copd.R
+++ THCIC_assembly_copd.R
@@ -9,25 +9,89 @@ library(stringr)
 
 
 #### outpatient data for COPD (J44): 2018-2020 ###############################################################
-#this data appears to have been created in 'OP THCIC clean up 10-2023.R'
-#apparently the files were delivered in several different files, so I'm picking it up at the point where only date still needs to be added
+#this follows 'OP THCIC clean up 10-2023.R'
+#apparently the data were delivered in several different files, so they need merging
+setwd("Z:/THCIC/Outpatient THCIC data 2018-2020")
 
-op2018_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2018_full.csv", guess_max = 10000) %>% 
-  filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) #doing the filtering at this step to save some memory
-op2019_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2019_full.csv", guess_max = 10000) %>% 
-  filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) 
-op2020_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2020_full.csv", guess_max = 10000) %>% 
-  filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) 
 
-op2018enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2018_enc.txt")
-op2019enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2019_enc.txt")
-op2020enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2020_enc.txt")
+################# 2018 data
+# "base" includes the main variables
+op2018full <- read_tsv("OP_2018_base.txt")%>% 
+  filter(str_detect(PRINC_DIAG_CODE, "^J44.")) #doing the filtering at this step to save some memory
 
-op2018 <- left_join(op2018_full, op2018enc) %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION))
-op2019 <- left_join(op2019_full, op2019enc) %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION))
-op2020 <- left_join(op2020_full, op2020enc) %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION))
+# "thcic_ID" includes the hospital THCIC ID 
+op2018hospitalID <- read_tsv("op_2018_THCIC_ID.txt")
+
+# "enc" includes the date of encounter variable STMT_PERIOD_FROM
+op2018enc <- read_tsv("op_2018_enc.txt")
+
+# linking THCIC hospital ID and data (merge the variables you want/need)
+op2018 <- left_join(op2018full, op2018hospitalID, by = "RECORD_ID") %>% 
+          left_join(., op2018enc) %>% 
+  mutate(Year = 2018) %>% 
+  filter(str_detect(PRINC_DIAG_CODE, "^J44."))
+
+################# 2019 data
+# "base" includes the main variables
+op2019full <- read_tsv("OP_2019_base.txt")%>% 
+  filter(str_detect(PRINC_DIAG_CODE, "^J44.")) #doing the filtering at this step to save some memory
+
+# "thcic_ID" includes the hospital THCIC ID 
+op2019hospitalID <- read_tsv("op_2019_THCIC_ID.txt")
+
+# "enc" includes the date of encounter variable STMT_PERIOD_FROM
+op2019enc <- read_tsv("op_2019_enc.txt")
+
+# linking THCIC hospital ID and data (merge the variables you want/need)
+op2019 <- left_join(op2019full, op2019hospitalID, by = "RECORD_ID") %>% 
+  left_join(., op2019enc) %>% 
+  mutate(Year = 2019) %>% 
+  filter(str_detect(PRINC_DIAG_CODE, "^J44."))
+
+
+################# 2020 data
+# "base" includes the main variables
+op2020full <- read_tsv("OP_2020_base.txt")%>% 
+     filter(str_detect(PRINC_DIAG_CODE, "^J44.")) #doing the filtering at this step to save some memory
+
+# "thcic_ID" includes the hospital THCIC ID 
+op2020hospitalID <- read_tsv("op_2020_THCIC_ID.txt")
+
+# "enc" includes the date of encounter variable STMT_PERIOD_FROM
+op2020enc <- read_tsv("op_2020_enc.txt")
+
+# linking THCIC hospital ID and data (merge the variables you want/need)
+op2020 <- left_join(op2020full, op2020hospitalID, by = "RECORD_ID") %>% 
+  left_join(., op2020enc) %>% 
+  mutate(Year = 2020) %>% 
+  filter(str_detect(PRINC_DIAG_CODE, "^J44."))
+
+
+# op2018_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2018_full.csv", guess_max = 10000) %>% 
+#   filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) #doing the filtering at this step to save some memory
+# op2019_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2019_full.csv", guess_max = 10000) %>% 
+#   filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) 
+# op2020_full <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/Filtered Data/op2020_full.csv", guess_max = 10000) %>% 
+#   filter(., str_detect(PRINC_DIAG_CODE, "^J44.")) 
+# 
+# op2018enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2018_enc.txt")
+# op2019enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2019_enc.txt")
+# op2020enc <- read_tsv("Z:/THCIC/Outpatient THCIC data 2018-2020/op_2020_enc.txt")
+
+### combine the dfs from each year
+dfs_to_keep <- c("op2018", "op2019", "op2020")
+rm(list=setdiff(ls(), dfs_to_keep))
+gc()
+
+op2018 <- op2018 %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION),
+                            RACE = as.character(RACE))
+op2019 <- op2019 %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION),
+                            RACE = as.character(RACE))
+op2020 <- op2020 %>% mutate(SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION),
+                            RACE = as.character(RACE))
 
 op_data_2018_2020 <- bind_rows(op2018, op2019, op2020) #test <- sample_n(op_data_2018_2020, 100)
+
 
 ## Cleaning up some errors in the THCIC data: using a script from RAZ for this
 # there are some errors, there should only be 0-5 options, see page 7 of RDF_OP.pdf for references
@@ -42,7 +106,7 @@ op_data_2018_2020$ETHNICITY <- factor(op_data_2018_2020$ETHNICITY, labels = c("M
 
 
 table(op_data_2018_2020$RACE, op_data_2018_2020$ETHNICITY)
-# there are 1074 cases that are Black and Latinx
+# there are 1898 cases that are Black and Latinx
 
 # creating a new variable that combines race and ethnicity
 # if a person is Black and Latinx, they are categorized as Black
@@ -57,29 +121,10 @@ op_data_2018_2020$PAT_ADDR_CENSUS_BLOCK_GROUP <- str_replace_all(op_data_2018_20
 #filling in any missing zeros at the end (numbers should be at least 11 digits long -- the 12th digit is an added level of specificity we can't use with census data)
 op_data_2018_2020$PAT_ADDR_CENSUS_BLOCK_GROUP <- stringi::stri_pad_right(op_data_2018_2020$PAT_ADDR_CENSUS_BLOCK_GROUP, 11, 0)
 
-
 #save 
 write_csv(op_data_2018_2020, "Z:/THCIC/Outpatient THCIC data 2018-2020/OPTexasCOPD18_20_enc_date.csv")
 #op_data_2018_2020 <- read_csv("Z:/THCIC/Outpatient THCIC data 2018-2020/OPTexasCOPD18_20_enc_date.csv")
 
-#PICK UP HERE NEXT TIME:
-#WHY IS 2018 23K CASES INSTEAD OF 50-60K??
-#looks like the op2018 file only has cases until the end of june?? need to dig deeper into why this is happening
-#need to check raw data here: Z:\THCIC\Outpatient THCIC data 2018-2020
-#first thought: it looks like I used files from the 'Filtered Data' folder, perhaps there was an error in the processing that was done?
-#
-op_data_2018_2020 %>% 
-  mutate(date_s = ymd(STMT_PERIOD_FROM),
-         year_s = year(date_s)) %>% 
-  group_by(as.character(year_s)) %>% 
-  summarize(n = n())
-
-
-op2019%>% 
-  mutate(date_s = ymd(STMT_PERIOD_FROM),
-         year_s = year(date_s)) %>% 
-  group_by(as.character(year_s)) %>% 
-  summarize(n = n())
 
 
 
@@ -131,7 +176,7 @@ op_data_2015_2017$RACE_ETHNICITY[op_data_2015_2017$ETHNICITY == "Non-Latinx" &  
 op_data_2015_2017$RACE_ETHNICITY[op_data_2015_2017$ETHNICITY == "Latinx"] <- "Latinx"
 op_data_2015_2017$RACE_ETHNICITY[op_data_2015_2017$RACE == "Black"] <- "Black"
 write.csv(op_data_2015_2017, "Z:/THCIC/Katz/op_copd_2015q4_2017.csv", row.names = FALSE)
-
+#op_data_2015_2017 <- read_csv("Z:/THCIC/Katz/op_copd_2015q4_2017.csv")
 
 
 ### outpatient: combining the outpatient 2015 q4 - 2017 data with the 2018-2020 data #################################################################################
@@ -255,6 +300,7 @@ ip_data_2015_2017$RACE_ETHNICITY[ip_data_2015_2017$ETHNICITY == "Non-Latinx" &  
 ip_data_2015_2017$RACE_ETHNICITY[ip_data_2015_2017$ETHNICITY == "Latinx"] <- "Latinx"
 ip_data_2015_2017$RACE_ETHNICITY[ip_data_2015_2017$RACE == "Black"] <- "Black"
 write.csv(ip_data_2015_2017, "Z:/THCIC/Katz/ip_copd_2015q4_2017.csv", row.names = FALSE)
+#ip_data_2015_2017 <- read_csv("Z:/THCIC/Katz/ip_copd_2015q4_2017.csv")
 
 ### inpatient: combining the inpatient 2015 q4 - 2017 data with the 2018-2020 data #################################################################################
 str(ip_data_2015_2017)
@@ -271,8 +317,10 @@ write_csv(ip_copd_2015q4_2020, "Z:/THCIC/Katz/ip_copd_2015q4_2020.csv")
 op_copd_2015q4_2020 <- read_csv("Z:/THCIC/Katz/op_copd_2015q4_2020.csv")
 ip_copd_2015q4_2020 <- read_csv("Z:/THCIC/Katz/ip_copd_2015q4_2020.csv")
 
-op_copd_2015q4_2020_join <- op_copd_2015q4_2020 %>% mutate(ip_op = "op")
-ip_copd_2015q4_2020_join <- ip_copd_2015q4_2020 %>% mutate(ip_op = "ip")
+op_copd_2015q4_2020_join <- op_copd_2015q4_2020 %>% mutate(ip_op = "op",
+                                                           SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION))
+ip_copd_2015q4_2020_join <- ip_copd_2015q4_2020 %>% mutate(ip_op = "ip",
+                                                           SOURCE_OF_ADMISSION = as.character(SOURCE_OF_ADMISSION))
 
 names(op_copd_2015q4_2020_join)
 names(ip_copd_2015q4_2020_join)
@@ -284,7 +332,7 @@ write_csv(op_ip_copd_2015q4_2020, "Z:/THCIC/Katz/op_ip_copd_2015q4_2020.csv")
 #sort(names(test))
 
 
-#visual test on number of cases over time
+###visual test on number of cases over time
 op_ip_copd_2015q4_2020 <- read_csv("Z:/THCIC/Katz/op_ip_copd_2015q4_2020.csv")
 names(op_ip_copd_2015q4_2020)
 str(op_ip_copd_2015q4_2020$STMT_PERIOD_FROM)
